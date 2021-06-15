5b673e9bf84e54403a5738be3825f36733cbf56e
Kristof Van Cauwenbergh <1167123+kristofvc@users.noreply.github.com>
2021-06-15 12:44:06 +0200

    Migration needs to be after the core migration

--- migrations/Version20210615113213.php
+++ migrations/Version20210615113213.php
@@ -9,7 +9,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
 * Auto-generated Migration: Please modify to your needs!
 */
-final class Version20210615093213 extends AbstractMigration
+final class Version20210615113213 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
