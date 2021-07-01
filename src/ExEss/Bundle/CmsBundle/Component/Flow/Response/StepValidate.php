82fb39f1db0890724aca05dc3a239500ae56eb5d
Kristof Van Cauwenbergh <1167123+kristofvc@users.noreply.github.com>
2021-07-01 16:29:55 +0200

    Issue #58 - Move cms-bundle to our monolith

--- src/ExEss/Bundle/CmsBundle/Component/Flow/Response/StepValidate.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace ExEss\Bundle\CmsBundle\Component\Flow\Response;
-
-class StepValidate
-{
-    public bool $result;
-
-    public array $errors = [];
-}
--- src/ExEss/Bundle/CmsBundle/Exception/NotAuthenticatedException.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace ExEss\Bundle\CmsBundle\Exception;
-
-class NotAuthenticatedException extends \DomainException
-{
-
-}
