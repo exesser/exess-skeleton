e9acaed47623204242a5ab5b889ba2e610176ef5
Bart Dens <bart@devart.be>
2021-07-09 14:01:28 +0200

    fix phpcs

--- tests/_support/FunctionalTester.php
+++ tests/_support/FunctionalTester.php
@@ -1,10 +1,6 @@
 <?php declare(strict_types=1);
 
 use ExEss\Bundle\CmsBundle\Component\Codeception\Traits\ServiceActions;
-use ExEss\Bundle\CmsBundle\Component\Flow\Action\Arguments;
-use ExEss\Bundle\CmsBundle\Component\Flow\Action\BackendCommandExecutor;
-use ExEss\Bundle\CmsBundle\Component\Flow\Action\Command;
-use ExEss\Bundle\CmsBundle\Component\Flow\Response\Model;
 
 /**
  * Inherited Methods
